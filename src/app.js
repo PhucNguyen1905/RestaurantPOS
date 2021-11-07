eee9a4d02deea16153fad7e923791128d7fb95b3
nguyendzaiii <nguyen08112001@kali.kali>
2021-11-06 23:02:25 -0400

    format app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 require("dotenv").config();
@@ -12,34 +12,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set View engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // // Set public folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Express session middleware
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: true,
-    saveUninitialized: true
-}))
-app.get('*', (req, res, next) => {
-    res.locals.cart = req.session.cart;
-    next();
-})
+    saveUninitialized: true,
+  })
+);
+app.get("*", (req, res, next) => {
+  res.locals.cart = req.session.cart;
+  next();
+});
 
 //Router
-const userRoute = require('./routes/user');
+const userRoute = require("./routes/user");
 const cartRoute = require("./routes/cart");
 
-app.use('/cart', cartRoute);
-app.use('/', userRoute);
-
+app.use("/cart", cartRoute);
+app.use("/", userRoute);
 
 let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
