0f62e89ffc9325cc1cfc89dff2378420b46ff97a
Nguyễn Trọng Phúc <ngtrongphuc1905@gmail.com>
2021-11-13 19:53:19 +0700

    Update again

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,6 @@ route.get("/", userController.viewHome);
 route.get("/menu", userController.viewMenu);
 route.get("/about", userController.viewAbout);
 route.get("/order", userController.viewOrder)
-route.get("/login", userController.viewLogin)
 route.get('/menu/:id', userController.viewDetail);
 route.post('/menu/filter', userController.viewMenuFilter);
 route.post('/menu/search', userController.viewMenuSearch);
